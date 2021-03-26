4bf260a081387b0a4e13693200f616737ac25396
Mgo <mgotm13@gmail.com>
2021-03-26 23:50:41 +0400

    Project structure modifications

---
+++ core/playground.py
@@ -0,0 +1,14 @@
+import chess
+
+board = chess.Board()
+
+print(board)
+
+board.push_san("e4")
+print('-----')
+print(board)
+print(board.legal_moves)
+print('-----')
+
+board.push_san("Nh6")
+print(board)
