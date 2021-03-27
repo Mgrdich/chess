99894b1a05b526f6b97c40cef21a4291222face8
Mgo <mgotm13@gmail.com>
2021-03-27 18:39:28 +0400

    initalizing Header and footer general template beside the Controller functionality

---
+++ static/js/controllers/MyApp.js
@@ -0,0 +1,10 @@
+let app = angular.module('myApp');
+
+app.config(['$rootScope', function ($rootScope) {}]);
+
+app.run(function () {});
+
+
+app.controller('ChessCtrl', ['$scope', '$http', function ($scope, $http) {
+
+}]);
