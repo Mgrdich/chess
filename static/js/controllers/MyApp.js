93034eeeaa6ebd43a7b1b0c1c4662707830782ce
Mgo <mgotm13@gmail.com>
2021-03-27 18:46:29 +0400

    chess board directive init +  minor code refactors

--- static/js/controllers/MyApp.js
+++ static/js/controllers/MyApp.js
@@ -1,10 +1,30 @@
 let app = angular.module('myApp');
 
-app.config(['$rootScope', function ($rootScope) {}]);
+app.config(['$rootScope', function ($rootScope) {
+}]);
 
-app.run(function () {});
+app.run(function () {
+});
 
 
 app.controller('ChessCtrl', ['$scope', '$http', function ($scope, $http) {
 
 }]);
+
+
+app.directive('chessBoard',['$parse', function ($parse) {
+    return {
+        restrict:'A',
+        scope:{
+
+        },
+        template:'<div></div>',
+        link:function (scope, element, attrs) {
+            // TODO bind events that are found in attribute
+
+            scope.$on('$destroy',function (){
+               // TODO unbind events
+            });
+        }
+    }
+}])
