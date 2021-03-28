36b78274f1f647c83caeaf842ca12283efc9caed
Mgo <mgotm13@gmail.com>
2021-03-28 21:28:38 +0400

    init chessboard working with the postions

--- static/js/controllers/MyApp.js
+++ static/js/controllers/MyApp.js
@@ -7,18 +7,15 @@ myApp.controller('ChessCtrl', ['$scope', '$http', function ($scope, $http) {
 
 myApp.directive('chessBoard', ['$parse', function ($parse) {
     return {
-        restrict: 'E',
-        scope: {
-            id: '@'
-        },
+        restrict: 'A',
         replace:true,
         template: '<div id="{{id}}" style="width: 400px"></div>',
         link: function (scope, element, attrs) {
-            // TODO bind events that are found in attribute
 
-            var board = ChessBoard(scope.id, {
+            var board = ChessBoard(element, {
                 draggable: true,
-                pieceTheme:'/static/img/chesspieces/{piece}.png'
+                pieceTheme:'/static/img/chesspieces/{piece}.png',
+                position:'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' //start position
             });
 
             scope.$on('$destroy', function () {
