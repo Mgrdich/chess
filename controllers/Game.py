88c8627faba5a4e3c13b40cd58d1ccc005034249
Mgo <mgotm13@gmail.com>
2021-03-30 01:43:17 +0400

    adding controller and class based approach in the controllers and views

--- controllers/Game.py
+++ controllers/Game.py
@@ -1,11 +1,13 @@
-from abc import ABC
-
 from flask import render_template
 from flask.views import View
+from run import app
 
 
-class Game(View, ABC):
+class Game(View):
     methods = ['GET']
 
     def dispatch_request(self):
         return render_template('main.html')
+
+
+app.add_url_rule('/game', view_func=Game.as_view('game'))
--- run.py
+++ run.py
@@ -3,19 +3,14 @@ from flask import Flask, render_template
 import chess
 import os
 
-from controllers import Game
-
 load_dotenv()  # take environment variables from .env.
 
 board = chess.Board()
 
 app = Flask(__name__)
 
-app.add_url_rule('/game', view_func=Game.as_view('game'))
 
-
-# Routes
-# in their current directories
+# Routes Default
 @app.route("/")
 def index():
     return render_template('index.html')
