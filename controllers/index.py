da164383eed266d3943e1ef6888a6118eaaf2df6
Mgo <mgotm13@gmail.com>
2021-03-30 20:52:20 +0400

    adding controller and class based approach in the controllers and views

--- controllers/index.py
+++ controllers/index.py
@@ -2,10 +2,9 @@ from flask import render_template, make_response
 from flask.views import View
 
 
-class Game(View):
+class HelloWorld(View):
     methods = ['GET']
 
     def dispatch_request(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('main.html'), 200, headers)
-
+        return make_response(render_template('index.html'), 200, headers)
--- run.py
+++ run.py
@@ -1,26 +1,22 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask
 import werkzeug
+from controllers.Game import Game
+from controllers.index import HelloWorld
+import chess
+import os
 
 werkzeug.cached_property = werkzeug.utils.cached_property
 
-from flask_restplus import Api, Resource
-import chess
-import os
 
 load_dotenv()  # take environment variables from .env.
 
 board = chess.Board()
 
 app = Flask(__name__)
-api = Api(app=app)
 
-
-@api.route('/')
-class HelloWorld(Resource):
-    def get(self):
-        return render_template('index.html')
-
+app.add_url_rule('/', view_func=HelloWorld.as_view('helloWorld'))
+app.add_url_rule('/game', view_func=Game.as_view('game'))
 
 # Web server config
 if __name__ == "__main__":
