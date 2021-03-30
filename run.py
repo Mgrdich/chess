e26b334f44546dfb7e87da52790d9294028e9957
Mgo <mgotm13@gmail.com>
2021-03-30 21:14:43 +0400

    error Handling in the backend

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 import werkzeug
 from controllers.Game import GameView
 from controllers.index import IndexView
@@ -8,7 +8,6 @@ import os
 
 werkzeug.cached_property = werkzeug.utils.cached_property
 
-
 load_dotenv()  # take environment variables from .env.
 
 board = chess.Board()
@@ -16,8 +15,16 @@ board = chess.Board()
 app = Flask(__name__)
 
 # Routes
+
 app.add_url_rule('/', view_func=IndexView.as_view('helloWorld'))
 app.add_url_rule('/game', view_func=GameView.as_view('game'))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    print(error)
+    return render_template('error.html', error=error), 404
+
 
 # Web server config
 if __name__ == "__main__":
