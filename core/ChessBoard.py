6c7e2aa2a20bb474e50cf4280af64b85b874d08c
Mgo <mgotm13@gmail.com>
2021-04-01 01:12:42 +0400

    Chessboard core functionality adding possible Moves from algebraic notation

--- core/ChessBoard.py
+++ core/ChessBoard.py
@@ -8,11 +8,17 @@ import chess
 # all of it particular to the board in the current object
 
 class ChessBoard:
-    def __init__(self, fen: ''):
-        self.board = chess.Board(fen)
+    def __init__(self, fen=''):
+        if fen:
+            self.board = chess.Board(fen)
+        else:
+            self.board = chess.Board()
 
     def isLegalMove(self, move) -> bool:
-        return move in self.board.legal_moves
+        return move in self.getLegalMoves()
+
+    def getLegalMoves(self):
+        return self.board.legal_moves
 
     def isCheck(self) -> bool:
         return self.board.is_check()
@@ -23,6 +29,12 @@ class ChessBoard:
     def isStaleMate(self) -> bool:
         return self.board.is_stalemate()
 
-    def possibleMoves(self, alg_notation: str):
-        arr = list(self.board.generate_legal_moves(from_mask=chess.parse_square(alg_notation)))
-        return arr
+    def printBoard(self):
+        print(self.board)
+
+    def possibleMoves(self, alg_notation: str) -> list:
+        return list(self.board.generate_legal_moves(from_mask=ChessBoard.getBitSquare(alg_notation)))
+
+    @staticmethod
+    def getBitSquare(alg_notation):
+        return chess.BB_SQUARES[chess.parse_square(alg_notation)]
