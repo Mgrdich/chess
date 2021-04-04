5b4f99def4f7e25a98c050d9dbca1271060bc51d
Mgo <mgotm13@gmail.com>
2021-04-04 16:05:13 +0400

    Adding error validation and Error utility class for validaiton

--- Util/ErrorUtil.py
+++ Util/ErrorUtil.py
@@ -6,35 +6,38 @@ from core.ChessUtil import ChessUtil
  example Query params
 """
 
+ERROR_KEYS = {
+    'not_valid_key': 'Not a valid key',
+    'is_required': 'This Field is required'
+}
+
 
 class ErrorUtil:
-    errors_keys = {
-        'not_valid_key': 'Not a valid key',
-        'is_required': 'This Field is required'
-    }
 
     def __init__(self):
         pass
 
     # maybe add any number of required parameters
-    def isRequired(self, name: str, item):
+    @staticmethod
+    def isRequired(name: str, item):
         if not item:
             obj = {
                 'status': 0,
                 'errors': {
-                    [name]: self.errors_keys['is_required']
+                    name: ERROR_KEYS['is_required']
                 }
             }
             return ErrorUtil.isInvalid(obj)
 
         return ErrorUtil.isValid()
 
-    def isAlgNotation(self, name: str, alg_not: str):
+    @staticmethod
+    def isAlgNotation(name: str, alg_not: str):
         if not ChessUtil.isAlgebraicNotation(alg_not):
             obj = {
                 'status': 0,
                 'errors': {
-                    [name]: self.errors_keys['not_valid_key']
+                    name: ERROR_KEYS['not_valid_key']
                 }
             }
             return ErrorUtil.isInvalid(obj)
@@ -52,5 +55,5 @@ class ErrorUtil:
     def isInvalid(res: object) -> object:
         return {
             'valid': False,
-            'response': Util.resJson(res),
+            'response': Util.resJson(res)
         }
--- controllers/MovesApi.py
+++ controllers/MovesApi.py
@@ -1,6 +1,6 @@
 from flask import make_response, jsonify
 from flask.views import MethodView
-from core.ChessUtil import ChessUtil
+from Util.ErrorUtil import ErrorUtil
 
 
 class MovesApi(MethodView):
@@ -9,29 +9,23 @@ class MovesApi(MethodView):
     @staticmethod
     def get(alg_move):
         """ Return the entire inventory collection """
-        if not ChessUtil.isAlgebraicNotation(alg_move):
-            return make_response(jsonify({
-                'status': 0,
-                'errors': {
-                    'alg_move': 'not a valid alg_move'
-                }
-            }
-            ), 200)
+
+        alg_validation = ErrorUtil.isAlgNotation('alg_move', alg_move)
+
+        if not alg_validation['valid']:
+            return alg_validation['response']
 
         # TODO make your request here
         return make_response(jsonify({'ks': 'emak'}), 200)
 
     @staticmethod
     def put(alg_move):
-        """ Return the entire inventory collection """
-        if not ChessUtil.isAlgebraicNotation(alg_move):
-            return make_response(jsonify({
-                'status': 0,
-                'errors': {
-                    'alg_move': 'not a valid alg_move'
-                }
-            }
-            ), 200)
+        """ Sets chess play in the Core """
+
+        alg_validation = ErrorUtil.isAlgNotation('alg_move', alg_move)
+
+        if not alg_validation['valid']:
+            return alg_validation['response']
 
         # TODO make your request here
         return make_response(jsonify({'ks': 'emak'}), 200)
